ce8eb971c431f5db114d85e96320ce7ea0b0d5b7
MarcosVKern <02949608051@senacrs.edu.br>
2025-11-06 01:14:55 +0000

    logico

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    int meuNumero = 7;
-    int meuOutroNumero;
+    int x = 5;
+    int y = 2;
 
-    printf("Digite um novo numero: ");
-    scanf("%d", &meuOutroNumero);
-
-    meuNumero = meuOutroNumero;
-
-    printf("%d", meuNumero);
+    printf("\n%d", x > y);
+    printf("\n%d", x == y);
+    printf("\n%d", x <= y);
+    printf("\n%d", x != y);
+    printf("\n%d", x < y && x > 4);
+    printf("\n%d", x == y || y > 0);
+    printf("\n%d", !(x > 4));
 
     return 0;
 }
+
+//int = 2 ou 4 bytes -- %d
+//float = 4 bytes -- %f / %.2f
+//double = 8 bytes -- %lf
+//char = 1 byte -- %c
